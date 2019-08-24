7d81d0743e21e183add25a765147a375cfd82d6a
cyliao <cyliao@iflytek.com>
2019-08-24 21:57:37 +0800

    add zuul and filter

--- src/main/java/com/xdhc/demo/DemoApplication.java
+++ src/main/java/com/xdhc/demo/DemoApplication.java
@@ -1,13 +1,40 @@
 package com.xdhc.demo;
 
+import com.xdhc.demo.filter.ErrorFilter;
+import com.xdhc.demo.filter.PostFilter;
+import com.xdhc.demo.filter.PreFilter;
+import com.xdhc.demo.filter.RouteFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@EnableZuulProxy
 public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
+	}
+
+	@Bean
+	public PreFilter preFilter() {
+		return new PreFilter();
+	}
+
+	@Bean
+	public PostFilter postFilter() {
+		return new PostFilter();
+	}
+
+	@Bean
+	public ErrorFilter errorFilter() {
+		return new ErrorFilter();
+	}
+
+	@Bean
+	public RouteFilter routeFilter() {
+		return new RouteFilter();
 	}
 
 }
